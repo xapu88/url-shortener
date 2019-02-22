971016750616691b69028d8b6b60cc3c88a74649
Hari Kalanj <hkalanj@gmail.com>
2019-02-22 12:12:04 +0100

    fix issue with attribute value delegating

--- app/Http/Controllers/UrlsController.php
+++ app/Http/Controllers/UrlsController.php
@@ -20,15 +20,16 @@ class UrlsController extends Controller
 
         $shortened = $url->shortenUrl($request['full_body']);
 
-        // TODO: I can't manage to avoid the error saying:
+        // TODO: Couldn't manage to avoid the error saying:
         // Field &#039;shortened_body&#039; doesn&#039;t have a default value
+        // (check first commit)
         // This issue took me about an hour of 3 hour time, else I would finish this endpoint by creating an url
         // and assigning user_id from Auth object of course instead hardcoding it like below
-        $url->create([
-            'full_body' => $request['full_body'],
-            'shortened_body' => $shortened,
-            'user_id' => 1
-        ]);
+        $url->full_body = $request['full_body'];
+        $url->shortened_body = $shortened;
+        $url->user_id = 1;
+
+        $url->save();
 
         return response()->json($url, 201);
     }
--- app/Url.php
+++ app/Url.php
@@ -9,6 +9,8 @@ class Url extends Model
     protected $fillable = ['full_body'];
 
     public function shortenUrl($body) {
-        return str_replace('http://', '', $body);
+        $shortened_body = str_replace('http://', '', $body);
+        $shortened_body = str_replace('https://', '', $shortened_body);
+        return $shortened_body;
     }
 }
